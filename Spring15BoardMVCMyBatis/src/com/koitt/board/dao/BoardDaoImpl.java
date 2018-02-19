e05f6a20bba08ff9b84c0dc77cd62c9f18107895
exemplaryer <exemplaryer@gmail.com>
2018-02-19 15:07:31 +0900

    Spring 15번 예제 문제 풀기 전 (오류 포함)

--- Spring15BoardMVCMyBatis/src/com/koitt/board/dao/BoardDaoImpl.java
+++ Spring15BoardMVCMyBatis/src/com/koitt/board/dao/BoardDaoImpl.java
@@ -2,9 +2,9 @@ package com.koitt.board.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.koitt.board.model.Board;
@@ -13,24 +13,20 @@ import com.koitt.board.model.BoardException;
 @Repository
 public class BoardDaoImpl implements BoardDao {
 	
+	private static final String MAPPER_NS = Board.class.getName();
+	
 	@Autowired
-	private JdbcTemplate template;
+	private SqlSession session;
 	
 	public BoardDaoImpl() {}
 
 	@Override
 	public void insert(Board board) throws BoardException {
-		try {
-			StringBuilder sql = new StringBuilder();
-			sql.append("INSERT INTO board (title, content, user_no, regdate) ");
-			sql.append("VALUES (?, ?, ?, CURDATE())");
-			
-			template.update(sql.toString(),
-					board.getTitle(),
-					board.getContent(),
-					board.getUserNo());
+		try {			
+			session.insert(MAPPER_NS + ".insert-board", board);
 			
 		} catch (Exception e) {
+			System.out.println(e.getMessage());
 			throw new BoardException(e.getMessage());
 		}
 	}
@@ -40,10 +36,10 @@ public class BoardDaoImpl implements BoardDao {
 		Board board = null;
 		
 		try {
-			String sql = "SELECT * FROM board WHERE no = ?";
-			board = template.queryForObject(sql, new BeanPropertyRowMapper<Board>(Board.class),
-					no);
+			board = session.selectOne(MAPPER_NS + ".select-board", no);
+			
 		} catch (Exception e) {
+			System.out.println(e.getMessage());
 			throw new BoardException(e.getMessage());
 		}
 		
