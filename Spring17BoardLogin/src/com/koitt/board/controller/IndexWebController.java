3242663c9640a06a1a2649428fb5e9eee6c002f8
exemplaryer <exemplaryer@gmail.com>
2018-02-26 09:46:20 +0900

    Spring 17번 예제 - 스프링 시큐리티 taglib 적용

--- Spring17BoardLogin/src/com/koitt/board/controller/IndexWebController.java
+++ Spring17BoardLogin/src/com/koitt/board/controller/IndexWebController.java
@@ -1,13 +1,8 @@
 package com.koitt.board.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.koitt.board.service.UsersService;
 
 @Controller
 public class IndexWebController {
