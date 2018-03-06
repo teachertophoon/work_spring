6407987baa75b749f89128a7050a346997f7e5d3
exemplaryer <exemplaryer@gmail.com>
2018-03-06 14:49:11 +0900

    Spring Batch 03 예제

---
+++ SpringBatch03CsvToMySQL/sql/ddl.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS raw_report;
+
+# CPC(Cost Per Click), CPM(Cost Per 1000 impressions)
+# 일자, 노출수, 클릭수, 수익
+CREATE TABLE raw_report (
+	date VARCHAR(10),
+	impressions VARCHAR(12),
+	clicks VARCHAR(10),
+	earning VARCHAR(10)
+);
+
+SELECT * FROM raw_report;
