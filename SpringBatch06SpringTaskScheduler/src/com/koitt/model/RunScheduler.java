ac97a3823d70dad6238fb9cf3fb42ed5fdfc50f2
exemplaryer <exemplaryer@gmail.com>
2018-03-07 15:10:26 +0900

    Spring Batch 06 - 2초마다 Job 실행하는 예제

--- SpringBatch06SpringTaskScheduler/src/com/koitt/model/RunScheduler.java
+++ SpringBatch06SpringTaskScheduler/src/com/koitt/model/RunScheduler.java
@@ -2,6 +2,12 @@ package com.koitt.model;
 
 import java.util.Date;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,24 +20,59 @@ import org.springframework.stereotype.Component;
 @Component	
 public class RunScheduler {
 	
-	@Scheduled(cron="*/1 * * ? * *")
-	public void run01() {
+//	@Scheduled(cron="*/1 * * ? * *")
+//	public void run01() {
+//		try {
+//			Thread.sleep(10000);	// 10초
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println("테스트 1 ..." + new Date());
+//	}
+//	
+//	@Scheduled(cron="*/1 * * ? * *")
+//	public void run02() {
+//		System.out.println("테스트 2 ..." + new Date());
+//	}
+//	
+//	@Scheduled(cron="*/1 * * ? * *")
+//	public void run03() {
+//		System.out.println("테스트 3 ..." + new Date());
+//	}
+	
+	@Autowired
+	private JobLauncher jobLauncher;
+	
+	@Autowired
+	private Job job;
+	
+	// 2초마다 Job 실행
+	@Scheduled(cron="*/2 * * ? * *")
+	public void run() {
 		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+			JobParameters param = new JobParametersBuilder()
+					// Job이 실행한 시간을 출력하기 위해 (콘솔창 빨간색 메시지 부분 확인)
+					.addString("date", new Date().toString())
+					.toJobParameters();
+			
+			JobExecution execution = jobLauncher.run(job, param);
+			System.out.println("종료 상태: " + execution.getStatus());
+			System.out.println("발생한 예외들: " + execution.getAllFailureExceptions());
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
-		System.out.println("테스트 1 ..." + new Date());
-	}
-	
-	@Scheduled(cron="*/1 * * ? * *")
-	public void run02() {
-		System.out.println("테스트 2 ..." + new Date());
-	}
-	
-	@Scheduled(cron="*/1 * * ? * *")
-	public void run03() {
-		System.out.println("테스트 3 ..." + new Date());
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
