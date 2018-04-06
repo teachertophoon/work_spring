d28751b4119db32f8adbf275965efd466eb1046f
exemplaryer <exemplaryer@gmail.com>
2018-04-06 11:22:40 +0900

    Ajax 예제

--- Spring19Ajax/src/com/koitt/model/Spring3AjaxRestController.java
+++ Spring19Ajax/src/com/koitt/model/Spring3AjaxRestController.java
@@ -1,7 +1,9 @@
 package com.koitt.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -63,5 +65,60 @@ public class Spring3AjaxRestController {
 		 * 입력받은 차량을 데이터베이스에 저장하면 되지만 현재 데이터베이스가 없으므로 
 		 * 출력까지만 구현
 		 */
+	}
+	
+	// 차량 한 대 수정하는 메소드
+	@ResponseBody
+	@RequestMapping(value="/car", method=RequestMethod.PUT)
+	public Map<String, Object> modifyCar(@RequestBody Car car) {
+		System.out.println("클라이언트로부터 전달받은 값: " 
+			+ car.getName() + " / " + car.getNumber());
+		
+		/*
+		 * 클라이언트로부터 전달받은 값으로 SQL문을 이용해 데이터베이스에 저장된 차량정보를
+		 * 수정하면 된다. 현재 데이터베이스는 구현하지 않았으므로 콘솔로 간단하게 출력까지만
+		 * 구현
+		 */
+		
+		// 데이터베이스에 BMW 차량만 저장되어 있다고 가정
+		List<Car> cars = new ArrayList<Car>();
+		cars.add(new Car("BMW", "20너1234"));
+		
+		// 리스트 검색하여 클라이언트로부터 입력받은 name과 동일한 차량이 있는지 검색
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		for (int i = 0; i < cars.size(); i++) {
+			if (car.getName().equals(cars.get(i).getName())) {
+				cars.get(i).setNumber(car.getNumber());
+				
+				resultMap.put("message", "차량 수정 성공!!");
+				return resultMap;
+			}
+		}
+		
+		resultMap.put("message", "차량 수정 실패!!");
+		return resultMap;
+	}
+	
+	// 차량 한 대 삭제하는 메소드
+	@ResponseBody
+	@RequestMapping(value="/car/{name}", method=RequestMethod.DELETE)
+	public Map<String, Object> removeCar(@PathVariable("name") String name) {
+		// 데이터베이스 대신 리스트로 표현
+		List<Car> cars = new ArrayList<>();
+		cars.add(new Car("BMW", "20너1234"));
+		cars.add(new Car("모닝", "11가1111"));
+		
+		// 데이터베이스에서 삭제할 차량 검색
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		for (int i = 0; i < cars.size(); i++) {
+			if (name.equals(cars.get(i).getName())) {
+				cars.remove(i);	// 리스트에서 차량 삭제
+				resultMap.put("message", "차량 삭제 성공!!");
+				return resultMap;
+			}
+		}
+		
+		resultMap.put("message", "차량 삭제 실패!!");
+		return resultMap;
 	}
 }
