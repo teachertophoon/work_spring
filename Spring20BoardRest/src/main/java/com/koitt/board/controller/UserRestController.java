51d397cfb170fb62eb03f59b473d3cabecb92eec
exemplaryer <exemplaryer@gmail.com>
2018-04-10 12:06:17 +0900

    Koitt Board 로그인 기능 (오류발생)

---
+++ Spring20BoardRest/src/main/java/com/koitt/board/controller/UserRestController.java
@@ -0,0 +1,70 @@
+package com.koitt.board.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+
+import com.koitt.board.model.Users;
+import com.koitt.board.model.UsersException;
+import com.koitt.board.service.UsersService;
+
+@RestController
+@RequestMapping("/rest")
+public class UserRestController {
+	
+	@Autowired
+	private UsersService usersService;
+	
+	// 사용자 로그인 @RequestMapping(value="/user/login", method=RequestMethod.POST)
+	@PostMapping(value="/user/login")
+	public ResponseEntity<Map<String, Object>> login(@RequestBody Users users, 
+			UriComponents ucBuilder) {
+		
+		System.out.println(users);
+		
+		// 아이디 존재 유무와 비밀번호 일치 여부 확인
+		try {
+			boolean isMatched = usersService.isPasswordMatched(
+					users.getEmail(), users.getPassword());
+			
+			if (isMatched) {
+				// Base64 인코딩 전 평문
+				String plainCredentials = users.getEmail() + ":" + users.getPassword();
+				
+				// 평문을 Base64로 인코딩
+				String base64Credentials = new String(Base64.encodeBase64(
+						plainCredentials.getBytes()));
+				
+				System.out.println("인코딩 한 값: " + base64Credentials);
+				
+				Map<String, Object> resultMap = new HashMap<>();
+				resultMap.put("credentials", base64Credentials);
+				
+				return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
+			}
+			else {
+				return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+			}
+			
+		} catch (UsersException e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
+
+
+
+
+
+
+
